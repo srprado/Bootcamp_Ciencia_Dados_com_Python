b7b6a0f6ffeb070b22b1f3c3ac671830703c3cec
sabrinarprado <sabryna13byna@gmail.com>
2025-11-22 20:07:45 -0300

    Entendendo sobre listas e seus métodos

---
+++ 0 - Códigos básicos/exe6.3_manipulacao_strings_multiplas_linhas.py
@@ -0,0 +1,19 @@
+nome = "Sabrina"
+
+mensagem = f'''
+Olá meu nome é {nome}. 
+    Esses são progrmas básicos em Pyhton.
+        Essa mensagem tem diferentes recuos.
+'''
+print(mensagem)
+
+print("""
+    ########## MENU ##########
+      
+      1 - Depósito
+      2 - Saque
+      3 - Extrato
+      0 - Sair
+
+    ##########################
+""")
